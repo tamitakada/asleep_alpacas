a174f3fd36fc9515637b263d3dace0ec9bce3dd5
Cabidge <cabbageboi69@gmail.com>
2021-10-30 13:53:14 -0400

    Added extra comments and conditionals to routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
@@ -8,31 +8,59 @@ def home():
 
 @app.route("/logout")
 def logout():
-    return "logout"
+    print("logout page...redirecting to homepage")
+    return redirect("/")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return "login"
+    if request.method == "POST":
+        # Check login
+        return redirect("/")
+    else:
+        # Display login page
+        return "login"
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return "register"
+    if request.method == "POST":
+        # Try to register account
+        return redirect("/")
+    else:
+        # Display register page
+        return "register"
 
-@app.route("/create")
+@app.route("/create", methods=["GET", "POST"])
 def create():
-    return "create"
+    if request.method == "POST":
+        # Add story to database
+        return redirect("/")
+    else:
+        # Display create page
+        return "create"
 
 @app.route("/discover")
 def discover():
     return "discover"
 
-@app.route("/story/<story_id>")
+@app.route("/story/<story_id>", methods=["GET", "POST"])
 def story(story_id):
     """
     Using angle brackets in the route means it'll pass the value
     of it as a parameter to the function
     """
-    return f"story with id of {story_id}"
+
+    user_has_contributed = True # placeholder variable
+
+    if user_has_contributed:
+        # Display full story
+        return f"story with id of {story_id}"
+    elif request.method == "POST":
+        # Add to story
+        # Display full story
+        return f"story with id of {story_id}"
+    else:
+        # Display edit page
+        return "edit"
 
 if __name__ == "__main__":
     app.debug = True
