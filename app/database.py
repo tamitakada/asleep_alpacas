136af062f66d35e8219c2ea92b93dc774577e645
zou-justin <justin82049@gmail.com>
2021-11-01 09:20:15 -0400

    using create table if not exists

--- app/database.py
+++ app/database.py
@@ -5,9 +5,9 @@ DB_FILE="database.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()
 
-c.execute("CREATE TABLE courses (id INTEGER, author_id INTEGER, title TEXT, full_story TEXT, last_update TEXT)")
+c.execute("CREATE TABLE IF NOT EXIST Stories (id INTEGER, author_id INTEGER, title TEXT, full_story TEXT, last_update TEXT)")
 #view Story
-c.execute("CREATE TABLE courses (id INTEGER, username TEXT, password TEXT)")
+c.execute("CREATE TABLE IF NOT EXIST users (id INTEGER, username TEXT, password TEXT)")
 #users
-c.execute("CREATE TABLE courses (user_id INTEGER, story_id INTEGER)")
+c.execute("CREATE TABLE IF NOT EXIST contributors (user_id INTEGER, story_id INTEGER)")
 #edit story
