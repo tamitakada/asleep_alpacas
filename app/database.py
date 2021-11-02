0a2e9351b37aa841279f47ffb869a3d2aafc8bde
Cabidge <cabbageboi69@gmail.com>
2021-11-02 17:37:01 -0400

    Clean up database.py

--- app/database.py
+++ app/database.py
@@ -1,27 +1,15 @@
 import sqlite3
 
 from flask import sessions
-import __init__
 
 DB_FILE="database.db"
-id = 0
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()
 
-c.execute("CREATE TABLE IF NOT EXIST Stories (id INTEGER, author_id INTEGER, title TEXT, full_story TEXT, last_update TEXT)")
-#view Story
+# Create tables if they don't exist
+c.execute("CREATE TABLE IF NOT EXIST stories (id INTEGER, author_id INTEGER, title TEXT, full_story TEXT, last_update TEXT)")
 c.execute("CREATE TABLE IF NOT EXIST users (id INTEGER, username TEXT, password TEXT)")
-if __init__.is_logged_in():
-    id = id + 1
-    command = f"""
-            INSERT INTO users VALUES
-              ({id},
-              {repr(sessions['user'])}
-              {repr(sessions['user'])})
-        """
-    c.execute(command);
-#users
-c.execute("CREATE TABLE IF NOT EXIST contributors (user_id INTEGER, story_id INTEGER)")
-#edit story
+c.execute("CREATE TABLE IF NOT EXIST contributions (user_id INTEGER, story_id INTEGER)")
+
 db.commit()
 db.close() 
